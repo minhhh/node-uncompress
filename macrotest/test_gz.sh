5defc3f64c24dca4150231f2c9e10a4859a27325
Ha.Minh <minhhh@minhhuyha.info>
2015-10-16 13:45:07 +0700

    update travis

--- macrotest/test_gz.sh
+++ macrotest/test_gz.sh
@@ -1,7 +1,7 @@
 node ../../lib/node-uncompress.js ../../test/1.gz -v
 
 if [ ! -f "1" ]; then
-    echo "Test case failed"
+    echo "Test case failed case 1"
     exit 1
 fi
 
@@ -10,26 +10,14 @@ cp ../../test/1.gz .
 node ../../lib/node-uncompress.js 1.gz -v
 
 if [ ! -f "1" ]; then
-    echo "Test case failed"
+    echo "Test case failed case 2"
     exit 1
 fi
 
 node ../../lib/node-uncompress.js ../../test/1.gz -v -d A
 
 if [ ! -f "A/1" ]; then
-    echo "Test case failed"
-    exit 1
-fi
-
-if [ ! -f "1.gz" ]; then
-    echo "Test case failed"
-    exit 1
-fi
-
-node ../../lib/node-uncompress.js ../../test/1.gz -v -d A
-
-if [ ! -f "A/1" ]; then
-    echo "Test case failed"
+    echo "Test case failed case 3"
     exit 1
 fi
 
